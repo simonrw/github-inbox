b1482ac388794edf0b18c09925a8da3bd4ddfa22
Simon Walker <s.r.walker101@googlemail.com>
2023-01-20 18:08:06 +0000

    Add darwin dependencies

--- flake.nix
+++ flake.nix
@@ -7,35 +7,51 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        linuxNativeBuildInputs = with pkgs; with xorg; [
+          pkg-config
+          python3
+          cmake
+          cargo
+          rustc
+          rustfmt
+          clippy
+        ];
+
+        linuxBuildInputs = with pkgs; with xorg; [
+          libxcb
+          libXcursor
+          libXrandr
+          libXi
+          libGL
+          libGLU
+          xorg.libX11
+          wayland
+          libxkbcommon
+          fontconfig
+        ];
+
+        darwinNativeBuildInputs = [
+        ];
+
+        darwinBuildInputs = with pkgs; [
+          libiconv
+          darwin.apple_sdk.frameworks.OpenGL
+          darwin.apple_sdk.frameworks.CoreServices
+          darwin.apple_sdk.frameworks.AppKit
+        ];
       in
       with pkgs; {
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with xorg; [
-            pkg-config
-            python3
-            cmake
-            cargo
-            rustc
-            rustfmt
-            clippy
-          ];
+          nativeBuildInputs = if pkgs.stdenv.isDarwin then darwinNativeBuildInputs else linuxNativeBuildInputs;
 
-          buildInputs = with xorg; [
-            libxcb
-            libXcursor
-            libXrandr
-            libXi
-            libGL
-            libGLU
-            xorg.libX11
-            wayland
-            libxkbcommon
-            fontconfig
-          ];
+          buildInputs = if pkgs.stdenv.isDarwin then darwinBuildInputs else linuxBuildInputs;
 
           shellHook = ''
             export LD_LIBRARY_PATH=/run/opengl-driver/lib/:${lib.makeLibraryPath ([libGL libGLU])}
           '';
+
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
         };
       }
     );
