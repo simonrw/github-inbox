6b1436610ffd3c14efbe131249cc767b05609ac6
Simon Walker <s.r.walker101@googlemail.com>
2023-01-20 22:41:49 +0000

    Handle fetching from github

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/tauri"
+    import { invoke } from "@tauri-apps/api/tauri";
 
-  let name = "";
-  let greetMsg = ""
+    let name = "";
+    let greetMsg = "";
+    let issues = "";
 
-  async function greet(){
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name })
-  }
+    async function greet() {
+        // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+        issues = await invoke("fetch_assigned_issues");
+        console.log(issues);
+    }
 </script>
 
 <div>
-  <div class="row">
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button on:click={greet}>
-      Greet
-    </button>
-  </div>
-  <p>{greetMsg}</p>
+    <div class="row">
+        <input
+            id="greet-input"
+            placeholder="Enter a name..."
+            bind:value={name}
+        />
+        <button on:click={greet}> Greet </button>
+    </div>
+    <p>{greetMsg}</p>
 </div>
+
