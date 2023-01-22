8b9cbe0731f03dc62efaf6ef0788dae915f7e28f
Simon Walker <s.r.walker101@googlemail.com>
2023-01-22 09:42:23 +0000

    Update flake to use linux specific things

--- flake.nix
+++ flake.nix
@@ -4,43 +4,61 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
 
-        linux-build-inputs = [
-          pkgs.webkitgtk
-        ];
-
-        macos-build-inputs = [
-          pkgs.libiconv
-        ] ++ (with pkgs.darwin.apple_sdk.frameworks;
-          [
-            Carbon
-            WebKit
-            AppKit
-          ]);
-
-        build-inputs =
-          if pkgs.stdenv.isDarwin then macos-build-inputs else linux-build-inputs;
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [
-            pkgs.pkg-config
-            pkgs.yarn
-            pkgs.nodePackages.prettier
-            pkgs.appimage-run
+          linux-build-inputs = [
+            pkgs.webkitgtk
           ];
-          buildInputs = build-inputs;
 
-          # TODO: temporary workaround
-          # https://github.com/NixOS/nixpkgs/issues/32580#issuecomment-350877197
-          WEBKIT_DISABLE_COMPOSITING_MODE = 1;
-          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-        };
-        packages.default = pkgs.callPackage ./default.nix { };
-        packages.github-inbox-bin = pkgs.stdenv.mkDerivation rec {
+          macos-build-inputs = [
+            pkgs.libiconv
+          ] ++ (with pkgs.darwin.apple_sdk.frameworks;
+            [
+              Carbon
+              WebKit
+              AppKit
+            ]);
+
+          build-inputs =
+            if pkgs.stdenv.isDarwin then macos-build-inputs else linux-build-inputs;
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.pkg-config
+              pkgs.yarn
+              pkgs.nodePackages.prettier
+            ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+              pkgs.appimage-run
+            ];
+
+            buildInputs = build-inputs;
+
+            # TODO: temporary workaround
+            # https://github.com/NixOS/nixpkgs/issues/32580#issuecomment-350877197
+            WEBKIT_DISABLE_COMPOSITING_MODE = 1;
+            RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          };
+          packages.default = pkgs.callPackage ./default.nix { };
+        }
+      ) // {
+      # System-specific things
+      packages.x86_64-linux.github-inbox-bin =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+          desktopItem = pkgs.makeDesktopItem {
+            categories = [ "Game" "AdventureGame" ];
+            desktopName = "Github Inbox";
+            exec = "github-inbox";
+            name = "github-inbox";
+          };
+
+        in
+        pkgs.stdenv.mkDerivation rec {
           pname = "github-inbox-bin";
           version = "0.0.0";
 
@@ -61,19 +79,13 @@
             pkgs.wrapGAppsHook
           ];
 
-          desktopItem = makeDesktopItem {
-            categories = [ "Game" "AdventureGame" ];
-            comment = description;
-            desktopName = long;
-            exec = "@out@/bin/${short}";
-            genericName = description;
-            icon = "scummvm";
-            name = name;
-          };
           unpackCmd = "dpkg-deb -x $curSrc source";
 
-          installPhase = "mv usr $out";
+          installPhase =
+            if pkgs.stdenv.isLinux then ''
+              mv usr $out;
+            '' else ''
+              '';
         };
-      }
-    );
+    };
 }
